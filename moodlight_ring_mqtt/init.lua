4cd8d6d6b5f7c35f36ea2fc4ebff0cd56d1dcd8c
valerio-vaccaro <valerio.vaccaro@gmail.com>
2015-07-04 19:02:00 +0200

    modify version

--- moodlight_ring_mqtt/init.lua
+++ moodlight_ring_mqtt/init.lua
@@ -1,48 +1,30 @@
--- Copyright (c) 2015 Sebastian Hodapp
+-- Based on code from Sebastian Hodapp
 -- https://github.com/sebastianhodapp/ESPbootloader
+-- https://github.com/carlsa/ESPbootloader
 
-
--- Change ssid and password of AP in configuration mode 
-ssid = "ESP" ..node.chipid()
+-- Change ssid and password of AP in configuration mode
+ssid = "ESP8266_"..node.chipid()
 psw  = "espconfig"
 
--- If GPIO0 changes during the countdown, launch config
-gpio.mode(3, gpio.INT)
-gpio.trig(3,"both",function()
-          tmr.stop(0)
-          dofile("run_config.lua")
-     end)
-     
-local countdown = 5
+if pcall(function ()
+	dofile("config.lc")
+end) then
+	print("Connecting to WIFI...")
 
-	tmr.alarm(1, 1000, 1, function() 
+	wifi.setmode(wifi.STATION)
+	wifi.sta.config(ssid,password)
+	wifi.sta.connect()
+
+	tmr.alarm(1, 1000, 1, function()
 		if wifi.sta.getip() == nil then
-			print("IP unavailable, waiting.") 
-		else 
+			print("IP unavaiable, waiting.")
+		else
 			tmr.stop(1)
 			print("Connected, IP is "..wifi.sta.getip())
 			dofile("run_program.lua")
-		end 
+		end
 	end)
 else
 	print("Enter configuration mode")
 	dofile("run_config.lua")
 end
-
-tmr.alarm(0,1000,1,function()
-     print(countdown)
-     countdown = countdown -1 
-     if (countdown == 0) then
-          gpio.mode(3,gpio.FLOAT)
-          tmr.stop(0)
-          if pcall(function () 
-          	dofile("config.lc")
-          end) then
-               dofile("run_program.lua")
-          else
-          	print("Enter configuration mode")
-          	dofile("run_config.lua")
-          end
-     end
-end)
-
