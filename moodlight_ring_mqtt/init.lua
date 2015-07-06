d6395ef91aa6f9d2b90ca0506f2c3be97f3cb1d5
valerio-vaccaro <valerio.vaccaro@gmail.com>
2015-07-06 10:21:47 +0200

    first working prototype

--- moodlight_ring_mqtt/init.lua
+++ moodlight_ring_mqtt/init.lua
@@ -6,6 +6,22 @@
 ssid = "ESP8266_"..node.chipid()
 psw  = "espconfig"
 
+BRIGHT     = 0.99
+ON         = BRIGHT * 255
+BUTTON_PIN = 3       -- GPIO0
+LED_PIN    = 4       -- GPIO2
+PIXELS     = 16
+TIME_ALARM = 25      -- 0.025 second, 40 Hz
+TIME_SLOW  = 500000  -- 0.500 second,  2 Hz
+
+RED   = string.char( 0, ON,  0)
+GREEN = string.char(ON,  0,  0)
+BLUE  = string.char( 0,  0, ON)
+WHITE = string.char(ON, ON, ON)
+BLACK = string.char( 0,  0,  0)
+
+ws2812.writergb(LED_PIN, WHITE:rep(PIXELS))
+
 if pcall(function ()
 	dofile("config.lc")
 end) then
@@ -17,14 +33,17 @@ end) then
 
 	tmr.alarm(1, 1000, 1, function()
 		if wifi.sta.getip() == nil then
+            ws2812.writergb(LED_PIN, RED:rep(PIXELS))
 			print("IP unavaiable, waiting.")
 		else
 			tmr.stop(1)
+            ws2812.writergb(LED_PIN, GREEN:rep(PIXELS))
 			print("Connected, IP is "..wifi.sta.getip())
 			dofile("run_program.lua")
 		end
 	end)
 else
+    ws2812.writergb(LED_PIN, BLUE:rep(PIXELS))
 	print("Enter configuration mode")
 	dofile("run_config.lua")
 end
